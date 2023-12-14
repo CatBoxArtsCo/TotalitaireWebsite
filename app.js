adf0281dd63f84ba657c30ad8a471334d2bcf089
Kaio Leandro <kaioleandrinn@gmail.com>
2023-12-14 02:03:24 -0300

    Alterações no codigo principal

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // ROTAS CRIADAS
 
-var indexRouter = require('./public/routes/index');
-var guiaRouter = require('./public/routes/guia');
-var timeRouter = require('./public/routes/time');
-var buymeRouter = require('./public/routes/buymeacoffe')
-var novidadesRouter = require('./public/routes/novidades');
+var indexRouter = require('./routes/index');
+var guiaRouter = require('./routes/guia');
+var timeRouter = require('./routes/time');
+var buymeRouter = require('./routes/buymeacoffe')
+var novidadesRouter = require('./routes/novidades');
 
 app.use('/', indexRouter);
 app.use('/guia', guiaRouter);
